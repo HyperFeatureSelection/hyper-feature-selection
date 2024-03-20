fc9b9c46946da9d564688679a8ec9e376cc572f3
Carlos Gonzalez Alvarez <carlosgonal@gmail.com>
2024-03-20 18:19:09 +0100

    Create principal folders

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hyper_feature_selection',
+    version='0.1.0',
+    description='A Python Package to select optimal feature in Machine Learning Models',
+    url='https://github.com/HyperFeatureSelection/hyper-feature-selection',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: C",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
